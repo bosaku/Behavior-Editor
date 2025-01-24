fea0a681a3bd851f2ef8a1deb7b8af7192c5ba9d
Richard Bryan Irwin <rbi.noli@gmail.com>
2025-01-24 12:00:37 -0700

    last from laptop

---
+++ Behavior-Editor/Scripts/Behavior/Conditions/TestCondition.cs
@@ -0,0 +1,13 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using SA;
+using UnityEngine;
+[CreateAssetMenu(fileName = "TestCondition", menuName = "Behavior/Conditions/TestCondition")]
+public class TestCondition : Condition
+{
+    public override bool CheckCondition(StateManager state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Behavior-Editor/Scripts/Editor/BehaviorEditor/Nodes/BaseNode.cs
+++ Behavior-Editor/Scripts/Editor/BehaviorEditor/Nodes/BaseNode.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor;
+﻿    using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
